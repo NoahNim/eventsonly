23e2663c36a7dcbcde1e9a02fe49eb528fe5cb74
NoahNim <nmedoff123@gmail.com>
2021-08-24 16:44:10 -0700

    add logo

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -5,11 +5,10 @@ function Home() {
     return (
         <dive className="home-container">
             <div className="homepage-container">
-                <div>
-                    <h1>Events Only</h1>
+                <div className="logo-home">
+                    <img alt="logo" src="https://prject-omega-events.s3.us-west-2.amazonaws.com/Events+Only.png"></img>
                 </div>
                 <div className="home-info">
-                    <h3>Welcome!</h3>
                     <p>Are you tired of trying to find events on sites like Facebook and Twitter, only to be flooded with a news feed that's unrelated? A news feed full of people posting what you consider to be nonsense and arguing. You only wish that you could find an event you and your friends are interested in, and connecting with them! Well now you can, with Events Only!</p>
                 </div>
             </div>
