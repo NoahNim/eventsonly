db664e2000fcb25c1472737e543233152e55a7cf
NoahNim <nmedoff123@gmail.com>
2021-08-24 13:11:55 -0700

    did model relations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -39,6 +39,9 @@ module.exports = (sequelize, DataTypes) => {
     };
     static associate(models) {
       // define association here
+      User.hasMany(models.Event, { foreignKey: 'userId' })
+      User.hasMany(models.Comment, { foreignKey: 'userId' })
+      User.hasMany(models.RSVP, { foreignKey: 'userId' })
     }
   };
   User.init(
