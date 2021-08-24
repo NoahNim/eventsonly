2f9c49d2ed892c5e64235731424ec6da900e5e36
NoahNim <nmedoff123@gmail.com>
2021-08-24 13:31:58 -0700

    changed logout to be in navbar, commented out profile button (unsure if I want to delete this just just)

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
+import { useSelector, useDispatch } from 'react-redux';
 import LoginFormModal from '../LoginFormModal';
+import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
+      // <ProfileButton user={sessionUser} />
+      <>
+        <button onClick={logout}>Log Out</button>
+      </>  
     );
   } else {
     sessionLinks = (
