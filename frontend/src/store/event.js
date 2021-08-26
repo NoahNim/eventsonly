4902c9d1b25066c8a1ad991c9b6518de56408c43
NoahNim <nmedoff123@gmail.com>
2021-08-26 10:44:47 -0700

    create action creators for event

--- frontend/src/store/event.js
+++ frontend/src/store/event.js
@@ -1,6 +1,31 @@
-// TYPE Constraints
+// TYPE CONSTANTS
 const LOAD = "events/load";
 const ADD = "events/add";
 const EDIT = "events/edit";
-const DELETE = "events/delete";
+const REMOVE = "events/remove";
+
+// ACTION CREATORS
+//LOAD Events
+const load = (event) => ({
+    type: LOAD,
+    event
+})
+
+//ADD Event
+const add =  (event) => ({
+    type: ADD,
+    event
+})
+
+//EDIT Event
+const edit = (event) => ({
+    type: EDIT.
+    event
+})
+
+// REMOVE Event
+const remove = (event) => ({
+    type: REMOVE,
+    event
+})
 
