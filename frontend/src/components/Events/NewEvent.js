b36e1f4e0034009e5fa958e3f02f5338369e8462
NoahNim <nmedoff123@gmail.com>
2021-08-26 17:06:03 -0700

    working on event creation form

--- frontend/src/components/Events/NewEvent.js
+++ frontend/src/components/Events/NewEvent.js
@@ -5,16 +5,43 @@ import { useHistory } from 'react-router-dom';
 function CreateEvent() {
     const dispatch = useDispatch();
     const history = useHistory();
-    const [name, setName] = useState();
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [eventPhoto, setEventPhoto] = useState("");
 
     const sessionUser = useSelector((state) => state.session.user);
     console.log('THIS IS THE USER ID', sessionUser.id)
 
+
+
     if (sessionUser) {
         return (
+            <div>
             <form>
-                this will be a form
-            </form>
+                <label>Event Name</label>
+                    <input
+                        type="test"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    ></input>
+                    <label>Description</label>
+                    <textarea
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    ></textarea>
+                    <label>Date</label>
+                    <input
+                        type="date"
+                        min="2021-01-31"
+                        max="2099-12-31"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    >
+                    </input>
+                </form>
+            </div>
         )
     }
 
