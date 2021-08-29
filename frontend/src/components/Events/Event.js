ce3e12b75f6d6a3810a457f41736bb3d288141b0
NoahNim <nmedoff123@gmail.com>
2021-08-29 11:24:33 -0700

    styling for new event creation

--- frontend/src/components/Events/Event.js
+++ frontend/src/components/Events/Event.js
@@ -40,7 +40,8 @@ function Event() {
                                     <li className="event-li">{event?.description}</li>
                                     <li className="event-li">{event?.date}</li>
                                 </div>
-                                {sessionUser?.id === event?.userId ? <div className = "event-buttons" >
+                                {
+                                    sessionUser?.id === event?.userId ? <div className="event-buttons" >
                                     <Link to={`/events/${event.id}/edit`}><button className="event-button event-edit"> [\] Edit [\]</button></Link>
                                     <button className="event-button event-delete" onClick={deleteEventHandler}> - Delete -</button>
                                 </div>
--- frontend/src/components/Events/NewEvent.js
+++ frontend/src/components/Events/NewEvent.js
@@ -36,9 +36,9 @@ function CreateEvent() {
                 const data = await res?.json();
                 setErrors(data?.errors);
             });
-        
+
         console.log(eventSubmit)
-        
+
         if (eventSubmit) {
             console.log('EVENT SUBMIT IN IF', eventSubmit)
             history.push('/events')
@@ -47,44 +47,54 @@ function CreateEvent() {
 
     if (sessionUser) {
         return (
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <div>
+            <div className="new-event-container">
+                <h2>Create Event</h2>
+                <form className="new-event-form" onSubmit={handleSubmit}>
+                    <div className="new-event-div">
                         <ul>
                             {errors?.map((error, idx) => <li key={idx}>{error}</li>)}
                         </ul>
                     </div>
-                    <label>Event Name</label>
-                    <input
-                        type="test"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+                    <div className="new-event-div">
+                        <label>Event Name</label>
+                        <input
+                            type="test"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         // required
-                    ></input>
-                    <label>Description</label>
-                    <textarea
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    ></textarea>
-                    <label>Date</label>
-                    <input
-                        type="date"
-                        min="2021-01-31"
-                        max="2099-12-31"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                    >
-                    </input>
-                    <label className="signup-label">
-                        Profile Photo URL
-                    </label>
-                    <input
-                        type="text"
-                        value={eventPhoto}
-                        onChange={(e) => setEventPhoto(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className="new-event-div">
+                        <label>Description</label>
+                        <textarea
+                            className="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        ></textarea>
+                    </div>
+                    <div className="new-event-div">
+                        <label>Date</label>
+                        <input
+                            type="date"
+                            min="2021-01-31"
+                            max="2099-12-31"
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                        >
+                        </input>
+                    </div>
+                    <div className="new-event-div">
+                        <label className="signup-label">
+                            Profile Photo URL
+                        </label>
+                        <input
+                            type="text"
+                            value={eventPhoto}
+                            onChange={(e) => setEventPhoto(e.target.value)}
                         // required
-                    />
-                    <button type="submit">Create</button>
+                        />
+                    </div>
+                    <button type="submit" className="new-event-button new-event-creator">Create</button>
                 </form>
             </div>
         )
