9b9452cbb39447f6bac0c20dde32157a3b74ccc7
NoahNim <nmedoff123@gmail.com>
2021-08-30 10:02:01 -0700

    delete event route

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -14,8 +14,8 @@ const validateComment = [
 ]
 
 //Get comments
-router.get("/events/:id(\\d+)'", asyncHandler(async (req, res) => {
-    const eventId = req.params.id
+router.get("/events/:eventId(\\d+)'", asyncHandler(async (req, res) => {
+    const eventId = req.params.eventId
     const event = await Event.findByPk(eventId)
     const comments = await Comment.findAll({
         include: event
@@ -25,9 +25,9 @@ router.get("/events/:id(\\d+)'", asyncHandler(async (req, res) => {
 }))
 
 //Create comment
-router.post("/events/:id(\\d+)/comment/new", require, validateComment, asyncHandler(async (req, res) => {
+router.post("/events/:eventId(\\d+)/comment/new", require, validateComment, asyncHandler(async (req, res) => {
     let userId = req.user.id;
-    let eventId = req.params.id;
+    let eventId = req.params.eventId;
 
     const { content } = req.body;
 
@@ -44,13 +44,14 @@ router.post("/events/:id(\\d+)/comment/new", require, validateComment, asyncHand
 
 
 //Edit comment
-router.put("/events/:id(\\d+)/comment/:id(\\d+)/edit", requireAuth, validateComment, asyncHandler(async (req, res) => {
+router.put("/events/:eventId(\\d+)/comment/:id(\\d+)/edit", requireAuth, validateComment, asyncHandler(async (req, res) => {
     let userId = req.user.id;
+    let eventId = req.params.eventId;
     let commentId = req.params.id;
 
     const comment = await Comment.findByPk(commentId);
 
-    const { content, eventId } = req.body;
+    const { content } = req.body;
 
     const updatedComment = {
         content,
@@ -61,4 +62,14 @@ router.put("/events/:id(\\d+)/comment/:id(\\d+)/edit", requireAuth, validateComm
     await comment.update(updatedComment)
 
     return res.json({ comment })
+}))
+
+//Delete comment
+router.delete("/events/:eventId(\\d+)/comment/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
+    let commentId = req.params.id;
+
+    const comment = await Comment.findByPk(commentId);
+
+    await comment.destroy()
+    return res.json();
 }))
