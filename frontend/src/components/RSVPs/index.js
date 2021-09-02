47dbd0018dd5eeb196b92a895bcc7c64485e9574
NoahNim <nmedoff123@gmail.com>
2021-09-02 14:08:47 -0700

    make front end rsvp fils

---
+++ frontend/src/components/RSVPs/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from 'react-router-dom'
+
+function RSVP() {
+    return (
+        <h3>TEST RSVP COMPONENT</h3>
+    )
+}
+
+export default RSVP
