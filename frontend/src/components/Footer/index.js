cb5b8b76ec8aff252a26a0cd88bebc970534981c
NoahNim <nmedoff123@gmail.com>
2021-09-02 01:02:10 -0700

    footer styling

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import './footer.css'
 
 function Footer() {
     return (
-        <div>
-            <h3>About</h3>
-            <ul>
-                <li>Created by: Noah Medoff</li>
-                <li><a href="https://www.linkedin.com/in/noah-medoff-6a5490116/">LinkedIn<img alt="linkedin" src="https://prject-omega-events.s3.us-west-2.amazonaws.com/linkedin-208-916919.png"></img></a></li>
-                <li><a href="https://github.com/NoahNim/eventsonly">GithHub<img alt="github" src="https://prject-omega-events.s3.us-west-2.amazonaws.com/github.png"></img></a></li>
+        <div className="footer">
+            <ul className="footer-list">
+                <li>Created by: Noah Medoff <a href="https://www.linkedin.com/in/noah-medoff-6a5490116/">LinkedIn <img alt="linkedin" src="https://prject-omega-events.s3.us-west-2.amazonaws.com/linkedin-208-916919.png"></img></a></li>
+                <li><a href="https://github.com/NoahNim/eventsonly">GithHub <img alt="github" src="https://prject-omega-events.s3.us-west-2.amazonaws.com/github.png"></img></a></li>
             </ul>
         </div>
     )
