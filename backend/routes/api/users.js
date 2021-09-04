2817bd6f6c2e242aaddbd97c348bae1a74c48398
NoahNim <nmedoff123@gmail.com>
2021-09-03 18:05:09 -0700

    turned comment creates into textareas, more error validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -12,25 +12,33 @@ const validateSignup = [
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Please provide a valid email.')
+    .isLength({ max: 100 })  
+  ,
   check('username')
     .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
+    .isLength({ min: 4, max: 100 })
+    .withMessage('Please provide a username with at least 4 characters but less than 100 characters.'),
   check('username')
     .not()
     .isEmail()
-    .withMessage('Username cannot be an email.'),
+    .withMessage('Username cannot be an email.')
+    .exists({ checkFalsy: true })
+    .withMessage("please input a username"),
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
     .withMessage('Password must be 6 characters or more.'),
   check('firstName')
     .exists({ checkFalsy: true })
-    .withMessage("Please put in a first name."),
+    .withMessage("Please put in a first name.")
+    .isLength({  max: 100 })
+    .withMessage("First names can not be more than 100 characters"),
   check('lastName')
     .exists({ checkFalsy: true })
-    .withMessage("Please put in a last name."),
+    .withMessage("Please put in a last name.")
+    .isLength({ max: 100 })
+    .withMessage("Last names can not be more than 100 characters"),
   handleValidationErrors,
 ];
 
