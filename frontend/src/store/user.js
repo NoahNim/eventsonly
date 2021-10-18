ca87ae7562096eddb286aad2fe3cc2ee703ed2be
NoahNim <nmedoff123@gmail.com>
2021-10-18 15:39:40 -0700

    make users action creators

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -2,6 +2,22 @@ import { csrfFetch } from "./csrf";
 
 // TYPE CONSTANTS
 const LOAD = "users/load";
-const DELETE = "users/delete";
+const REMOVE = "users/delete";
 const EDIT = "users/edit";
 
+//ACTION CREATORS
+
+const load = (user) => ({
+    type: LOAD,
+    user
+})
+
+const remove = (user) => ({
+    type: REMOVE,
+    user
+})
+
+const edit = (user) => ({
+    type: EDIT,
+    user
+})
