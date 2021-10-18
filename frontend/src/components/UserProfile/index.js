77a39624368e4030fe19ada9d3a1258806a0ca1a
NoahNim <nmedoff123@gmail.com>
2021-10-18 14:59:25 -0700

    create base function for user profile

---
+++ frontend/src/components/UserProfile/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function UserProfile() {
+    return (
+        <div>
+            USER PROFILE GOES HERE
+        </div>
+    )
+}
+
+export default UserProfile
