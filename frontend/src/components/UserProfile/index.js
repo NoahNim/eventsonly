537d2a9f5207fd445a12d896b67c2ca99bfaef85
NoahNim <nmedoff123@gmail.com>
2021-10-19 03:13:20 -0700

    add imports to user index.js

--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams, Link } from 'react-router-dom';
+import './UserProfile.css'
 
 function UserProfile() {
+
+
     return (
         <div>
             USER PROFILE GOES HERE
