58855b0dcacaa5d7ee457f9a09f5ccbc85d4bef6
NoahNim <nmedoff123@gmail.com>
2021-10-19 03:18:21 -0700

    add getUser import

--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams, Link } from 'react-router-dom';
+import { getUser } from "../../store/user";
 import './UserProfile.css'
 
 function UserProfile() {
