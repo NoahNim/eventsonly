2f76a1fd18204aeea42dc49e9aab121fa6815e9e
Noah Medoff <nmedoff123@gmail.com>
2023-01-10 13:33:10 -0800

    get store index.js converted to typescript

--- frontend/src/store/index.tsx
+++ frontend/src/store/index.tsx
@@ -1,9 +1,16 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import session from './session'
 import events from './event';
 import comments from './comment';
 import users from './user';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 const rootReducer = combineReducers({
   session,
@@ -12,19 +19,21 @@ const rootReducer = combineReducers({
   users
 });
 
-let enhancer;
+let enhancer: Function;
 
 if (process.env.NODE_ENV === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
   const logger = require('redux-logger').default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
+const configureStore = (preloadedState = {}) => {
+  return createStore(rootReducer, preloadedState, enhancer());
 };
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = ReturnType<typeof configureStore>["dispatch"]
 
 export default configureStore;
